8c5174209bc92f6356c355548a73fdf7e9651145
ashutoshsharmaindia <ashu.sharma.india@gmail.com>
2015-09-26 12:31:01 -0700

    Update BookXDriver.java
    Updating for newer Hadoop API

--- StepByStepMRGuide_BookCrossing/src/com/orzota/bookx/BookXDriver.java
+++ StepByStepMRGuide_BookCrossing/src/com/orzota/bookx/BookXDriver.java
@@ -5,48 +5,54 @@ package com.orzota.bookx;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 /**
  * @organisaion Orzota, Inc.
  * @author varadmeru
  */
-public class BookXDriver {
-	public static void main(String[] args) {
-		JobClient client = new JobClient();
-		// Configurations for Job set in this variable
-		JobConf conf = new JobConf(com.orzota.bookx.BookXDriver.class);
+ /*
+ Update history : Updating for newer Hadoop API
+ */
+public class BookXDriver
+{
+	public static void main(String[] args) throws Exception
+	{
+		@SuppressWarnings("deprecation")
+		Job job = new Job();
 		
-		// Name of the Job
-		conf.setJobName("BookCrossing1.0");
-		
-		// Data type of Output Key and Value
-		conf.setOutputKeyClass(Text.class);
-		conf.setOutputValueClass(IntWritable.class);
-		
-		// Setting the Mapper and Reducer Class
-		conf.setMapperClass(com.orzota.bookx.BookXMapper.class);
-		conf.setReducerClass(com.orzota.bookx.BookXReducer.class);
 
-		// Formats of the Data Type of Input and output
-		conf.setInputFormat(TextInputFormat.class);
-		conf.setOutputFormat(TextOutputFormat.class);
+		// Name of the Job
+		job.setJobName("BookCrossing1.0");
 		
+		job.setJarByClass(BookXDriver.class);
+
+		// Data type of Output Key and Value
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// Setting the Mapper and Reducer Class
+		job.setMapperClass(com.orzota.bookx.BookXMapper.class);
+		job.setReducerClass(com.orzota.bookx.BookXReducer.class);
+
+		// Formats of the Data Type of Input and output			
+		//job.setInputFormatClass(TextInputFormat.class);
+		//job.setOutputFormatClass(TextOutputFormat.class);
+			
+
 		// Specify input and output DIRECTORIES (not files)
-		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		FileInputFormat.addInputPath( job, new Path(args[0]));
+		FileOutputFormat.setOutputPath( job, new Path(args[1]));
+		//job.submit();
+		//job.waitForCompletion(true);
 		
-		client.setConf(conf);
-		try {
+		
 			// Running the job with Configurations set in the conf.
-			JobClient.runJob(conf);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.exit(job.waitForCompletion(true)?0:1);
+		
 	}
 }
